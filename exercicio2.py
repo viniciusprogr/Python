fdab6636f4b2362e148528f19c5e89e551c2bbe8
viniciusprogr <vnsrodrigues10@gmail.com>
2026-06-05 14:46:14 -0300

    f string para formatação

--- exercicio2.py
+++ exercicio2.py
@@ -6,3 +6,13 @@ print('nome:', nome)
 print('altura:', altura)
 print('peso:', peso)
 print('IMC:', imc)
+
+# Formatação de string usando f-string para exibir as informações de forma organizada
+
+linha1 = f'Olá, meu nome é {nome}' 
+linha2 = f' tenho {altura:.2f} metros de altura e peso {peso} kg.'
+linha3 = f'Meu IMC é {imc:.2f}.' 
+
+print(linha1)
+print(linha2)
+print(linha3)
